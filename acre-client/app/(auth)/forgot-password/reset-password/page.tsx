01efd4d9d72d4c390d851a4505eebcc2e0daf150
7TIN <prasadgujarathi619@gmail.com>
2025-08-11 03:44:36 +0530

    feat (auth): forgot-password added

---
+++ acre-client/app/(auth)/forgot-password/reset-password/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React, { Suspense } from "react";
+import ResetPasswordForm from "./resetPasswordForm"; // Move your entire form logic into this component.
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResetPasswordForm />
+    </Suspense>
+  );
+}
