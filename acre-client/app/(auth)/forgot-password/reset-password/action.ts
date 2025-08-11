e2950bddcedcd72be300815bfc40853696007617
7TIN <prasadgujarathi619@gmail.com>
2025-08-11 18:49:05 +0530

    feat (Auth): full functional auth added

--- acre-client/app/(auth)/forgot-password/reset-password/action.ts
+++ acre-client/app/(auth)/forgot-password/reset-password/action.ts
@@ -1,6 +1,5 @@
 "use server";
 
-// import { z } from "zod";
 import { passwordMatchSchema } from "@/validation/passwordMatchSchema";
 import { createClient } from "@/utils/supabase/server";
 
@@ -12,31 +11,6 @@ export const resetPasswordFunc = async ({
   passwordConfirm: string;
 }) => {
     
-//     {
-//   const newUserSchema = z.object({
-//     password: z.string().min(6),
-//     passwordConfirm: z.string().min(6),
-//   });
-
-//   const newUserValidation = newUserSchema.safeParse({
-//     password,
-//     passwordConfirm,
-//   });
-
-//   if (!newUserValidation.success) {
-//     return {
-//       error: true,
-//       message: newUserValidation.error.issues[0]?.message ?? "An error occurred",
-//     };
-//   }
-
-//   if (password !== passwordConfirm) {
-//     return {
-//       error: true,
-//       message: "Passwords do not match",
-//     };
-//   }
-
 const validation = passwordMatchSchema.safeParse({
     password,
     passwordConfirm,
