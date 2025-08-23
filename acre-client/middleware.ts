5fab5b40ab576bbd9f17d62ded258cd625af5321
7TIN <prasadgujarathi619@gmail.com>
2025-08-23 19:57:39 +0530

    api routes added

--- acre-client/middleware.ts
+++ acre-client/middleware.ts
@@ -56,6 +56,13 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|auth/callback|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+     /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|auth/callback|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
